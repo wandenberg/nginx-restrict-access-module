773be9225369994d2a4067ba880545209c2952bd
Wandenberg <wandenberg@gmail.com>
2015-06-28 22:13:12 -0300

    fix ipv6 support

--- test/check_restrict_access_spec.rb
+++ test/check_restrict_access_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path('spec_helper', File.dirname(__FILE__))
 
 describe "check restrict access module" do
-  it "should check reverse DNS" do
+  it "should check reverse DNS for ipv4" do
     nginx_run_server({allow_hosts: ["^crawl-[0-9\-]*\.googlebot\.com$"], deny_hosts: ["all"], restrict_access_address: "$http_x_origin_ip"}, timeout: 10) do
       EventMachine.run do
         sub = EventMachine::HttpRequest.new("#{nginx_address}/allows_first").get(head: {"x-origin-ip" => "66.249.64.10"})
@@ -18,7 +18,7 @@ describe "check restrict access module" do
     end
   end
 
-  it "should be possible ignore reverse DNS" do
+  it "should be possible ignore reverse DNS for ipv4" do
     nginx_run_server({allow_hosts: ['"^.*\.ptr\.globo\.com$" no_reverse_dns'], deny_hosts: ["all"], restrict_access_address: "$http_x_origin_ip"}, timeout: 10) do
       EventMachine.run do
         sub = EventMachine::HttpRequest.new("#{nginx_address}/allows_first").get(head: {"x-origin-ip" => "66.249.64.10"})
@@ -43,6 +43,23 @@ describe "check restrict access module" do
           expect(sub).to be_http_status(403)
 
           EventMachine.stop
+        end
+      end
+    end
+  end
+
+  it "should check reverse DNS for ipv6" do
+    nginx_run_server({allow_hosts: ["^p[0-9A-F]*\.dip0.t-ipconnect.de$"], deny_hosts: ["all"], restrict_access_address: "$http_x_origin_ip"}, timeout: 10) do
+      EventMachine.run do
+        sub = EventMachine::HttpRequest.new("#{nginx_address}/allows_first").get(head: {"x-origin-ip" => "2003:45:442c:8b3c:f945:b364:241e:420a"})
+        sub.callback do
+          expect(sub).to be_http_status(404)
+
+          sub = EventMachine::HttpRequest.new("#{nginx_address}/allows_first").get(head: {"x-origin-ip" => "2001:1388:5e01:9f09:b652:7dff:fe0c:c502"})
+          sub.callback do
+            expect(sub).to be_http_status(403)
+            EventMachine.stop
+          end
         end
       end
     end
